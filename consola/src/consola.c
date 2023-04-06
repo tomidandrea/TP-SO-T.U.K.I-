e4033817a8aa1e7d8d7f60a7bb1142ca5a687e9c
alejosandrini <alejo.sandrini@gmail.com>
2023-04-06 18:16:42 +0000

    seguimos empezando

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -1,12 +1,33 @@
 #include <consola.h>
 
 int main(int argc, char** argv[]) {
-    puts("Hello world!!");
-    // hacer una funcion mas linda
+    t_log* logger;
     t_config* config;
+    logger = iniciar_logger();
+    config = iniciar_config(argv[1]);
+    log_info(logger, "Inicio consola \n");
 
-    config = config_create("consola.config");
 
     return 0;
 }
 
+t_config* iniciar_config (char* path){
+	t_config* nuevo_config = config_create(path);
+	if(nuevo_config== NULL){
+		printf("No se pudo crear el config\n");
+	}
+
+	return nuevo_config;
+}
+
+t_log* iniciar_logger(void)
+{
+	char* file = "consola.log";
+	char *process_name = "CONSOLA";
+	bool is_active_console = true;
+	t_log_level level = LOG_LEVEL_INFO;
+	t_log* nuevo_logger = log_create(file, process_name,
+			is_active_console, level);
+
+	return nuevo_logger;
+}
--- consola/src/consola.h
+++ consola/src/consola.h
@@ -8,4 +8,7 @@
 #include<commons/config.h>
 #include<readline/readline.h>
 
+t_config* iniciar_config (char* path);
+t_log* iniciar_logger(void);
+
 #endif
